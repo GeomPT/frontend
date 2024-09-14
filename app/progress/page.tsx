6f163883d5dda6bafb153910edf279ab3d54ecf3
Allen Liu <al3544@princeton.edu>
2024-09-14 15:43:25 -0400

    graph fix

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { Line } from 'react-chartjs-2'; 
+import "chart.js/auto";
 import {CategoryScale} from 'chart.js'; 
 import { Chart as ChartJS } from 'chart.js/auto'
 import { Chart }            from 'react-chartjs-2'
