7866e562f2fbf3c8f079f7464e26c2c7eaf599dc
WhoTho <alesandrogopal@gmail.com>
2024-09-15 03:11:05 -0400

    design changes to exercise page

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -84,51 +84,86 @@ export default function Component() {
                     GeomPT Progress
                 </h1> */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    {videoData.map((video) => (
-                        <Link key={video.id} href={`/exercises/${video.id}`}>
-                            <Card
+                    {videoData.map((video) => {
+                        const progressValue =
+                            (video.id * 27456783 +
+                                ((video.id * 2364372) % 325) +
+                                video.id -
+                                ((video.id * 2371) % 236)) %
+                            (100 - video.id * 3);
+                        return (
+                            <Link
                                 key={video.id}
-                                className="bg-white shadow-lg hover:shadow-xl flex-shrink-0 relative duration-300 transform hover:scale-105"
+                                href={`/exercises/${video.id}`}
                             >
-                                <CardHeader className="flex flex-col items-center text-center">
-                                    <h2 className="text-xl font-semibold mb-4">
-                                        {video.title}
-                                    </h2>
-                                    <div className="h-[100px] w-full flex items-center justify-center mb-4">
-                                        <Image
-                                            src={`/images/v${video.id}.jpg`}
-                                            alt={`Video ${video.title}`}
-                                            width={178}
-                                            height={100}
-                                            className="max-h-[100px] w-auto"
-                                            style={{ objectFit: "contain" }}
+                                <Card
+                                    key={video.id}
+                                    className="bg-white shadow-lg hover:shadow-xl flex-shrink-0 relative duration-300 transform hover:scale-105"
+                                >
+                                    <CardHeader className="flex flex-col items-center text-center">
+                                        <h2 className="text-xl font-semibold mb-4">
+                                            {video.title}
+                                        </h2>
+                                        <div className="h-[100px] w-full flex items-center justify-center mb-4">
+                                            <Image
+                                                src={`/images/v${video.id}.jpg`}
+                                                alt={`Video ${video.title}`}
+                                                width={178}
+                                                height={100}
+                                                className="max-h-[100px] w-auto"
+                                                style={{ objectFit: "contain" }}
+                                            />
+                                        </div>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <p className="text-gray-600 mb-4">
+                                            {video.description}
+                                        </p>
+                                        <div className="flex flex-wrap gap-2 mb-4">
+                                            {video.technologies.map(
+                                                (tech, index) => {
+                                                    let color = "text-gray-700";
+                                                    if (tech === "Easy") {
+                                                        color =
+                                                            "text-green-700";
+                                                    } else if (
+                                                        tech === "Medium"
+                                                    ) {
+                                                        color =
+                                                            "text-yellow-700";
+                                                    } else if (
+                                                        tech === "Hard"
+                                                    ) {
+                                                        color = "text-red-700";
+                                                    }
+
+                                                    const className = `px-2 py-1 bg-gray-200 rounded-full text-xs ${color}`;
+
+                                                    return (
+                                                        <span
+                                                            key={index}
+                                                            className={
+                                                                className
+                                                            }
+                                                        >
+                                                            {tech}
+                                                        </span>
+                                                    );
+                                                }
+                                            )}
+                                        </div>
+                                        <Progress
+                                            value={progressValue}
+                                            className="h-2"
                                         />
-                                    </div>
-                                </CardHeader>
-                                <CardContent>
-                                    <p className="text-gray-600 mb-4">
-                                        {video.description}
-                                    </p>
-                                    <div className="flex flex-wrap gap-2 mb-4">
-                                        {video.technologies.map(
-                                            (tech, index) => (
-                                                <span
-                                                    key={index}
-                                                    className="px-2 py-1 bg-gray-200 text-gray-700 rounded-full text-xs"
-                                                >
-                                                    {tech}
-                                                </span>
-                                            )
-                                        )}
-                                    </div>
-                                    <Progress value={33} className="h-2" />
-                                    <p className="text-sm text-gray-500 mt-2">
-                                        33% complete
-                                    </p>
-                                </CardContent>
-                            </Card>
-                        </Link>
-                    ))}
+                                        <p className="text-sm text-gray-500 mt-2">
+                                            {progressValue}% complete
+                                        </p>
+                                    </CardContent>
+                                </Card>
+                            </Link>
+                        );
+                    })}
                 </div>
             </div>
         </div>
