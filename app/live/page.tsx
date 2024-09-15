a2f072fda3c3e65892e249249fbbe6db7bd85f8a
Hector Astrom <hector.astrom@gmail.com>
2024-09-14 21:54:18 -0400

    + dropdown in /live to select joint type

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import LiveVideoFrame from "@/components/ui/LiveVideoFrame";
+import {useState} from 'react';
 
 export default function LivePage() {
+  const [processingType, setProcessingType] = useState("elbow");
+
   return (
     <>
       <h1 className="text-xl font-semibold mb-4">Video Stream:</h1>
@@ -10,8 +13,14 @@ export default function LivePage() {
         streaming={true}
         width={1280}
         height={720}
-        processingType="shoulder"
+        processingType={processingType}
       />
+
+      <select className="mt-4 shadow-sm" value={processingType} onChange={(e) => setProcessingType(e.target.value)}>
+        <option value="shoulder">Shoulder</option>
+        <option value="elbow">Elbow</option>
+        <option value="hip">Hip</option>
+      </select>
     </>
   );
 }
